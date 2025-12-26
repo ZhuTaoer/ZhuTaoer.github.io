95e7be6c3877f56666a5496c7c81f9c447929cfd
Tao Zhu <zhutao.phd@gmail.com>
2025-12-26 14:40:31 +0800

    Site updated: 2025-12-26 14:40:31

--- js/time-insert.js
+++ js/time-insert.js
@@ -1,28 +1,30 @@
-
-var  newDiv  =  document.createElement("div");
-
-newDiv.innerHTML  =  `
-
-      <span id="time"></span>
-
-      <script src="/js/time.js"></script>
-
-      `;
+var newDiv = document.createElement("div");
+newDiv.innerHTML = '<span id="time"></span>';
 
 document.addEventListener("DOMContentLoaded", function() {
+      var div1 = document.getElementsByClassName("statistics")[0];
+      var div2 = document.getElementsByClassName("beian")[0];
 
- var  div1  =  document.getElementsByClassName("statistics")[0];
-
- var  div2  =  document.getElementsByClassName("beian")[0];
-
- /*调试检查*/
- console.log(div1); 
- console.log(div2);  
- 
- if (div1  &&  div2) {
-
- div1.parentNode.insertBefore(newDiv, div2);
-
-    }
-
+      if (div1 && div2) {
+            div1.parentNode.insertBefore(newDiv, div2);
+            var timeEl = document.getElementById("time");
+            if (timeEl) {
+                  function pad(n) { return n < 10 ? "0" + n : n; }
+                  function formatNow() {
+                        var d = new Date();
+                        var y = d.getFullYear();
+                        var m = pad(d.getMonth() + 1);
+                        var day = pad(d.getDate());
+                        var h = pad(d.getHours());
+                        var min = pad(d.getMinutes());
+                        var s = pad(d.getSeconds());
+                        return y + "-" + m + "-" + day + " " + h + ":" + min + ":" + s;
+                  }
+                  function tick() {
+                        timeEl.textContent = formatNow();
+                  }
+                  tick();
+                  setInterval(tick, 1000);
+            }
+      }
 });
