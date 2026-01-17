28071a4ed588bb0964f3a67dd961422095c9e96a
Tao Zhu <zhutao.phd@gmail.com>
2026-01-17 15:44:15 +0800

    Site updated: 2026-01-17 15:44:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/01/02/Nguyen/","2026/01/03/Position Control in Lithographic Equipment/","2026/01/03/life/","2026/01/02/page/","2026/01/03/哒哒/","2026/01/05/hello-world/","2026/01/03/哲学思辨/"];function toRandomPost(){
+var posts=["2026/01/02/Nguyen/","2026/01/03/Position Control in Lithographic Equipment/","2026/01/05/hello-world/","2026/01/02/page/","2026/01/03/life/","2026/01/03/哒哒/","2026/01/03/哲学思辨/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
