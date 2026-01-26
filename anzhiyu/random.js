4b6a3e6d9cb66552e27cf8c4fc67aafa1e7e7219
Tao Zhu <zhutao.phd@gmail.com>
2026-01-26 20:14:18 +0800

    Site updated: 2026-01-26 20:14:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/26/Zhang Yitang/","2026/01/26/Beatify-Blogs-Chapter-1/"];function toRandomPost(){
+var posts=["2026/01/26/Beatify-Blogs-Chapter-1/","2025/12/26/Zhang Yitang/"];function toRandomPost(){
     window.location.href='/'+posts[Math.floor(Math.random() * posts.length)];
   };
